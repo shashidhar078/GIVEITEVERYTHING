8017cedb020a2d7c173d1b9c25c8efaf90f393ce
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2025-12-27 14:55:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prefix to Postfix Conversion/prefix-to-postfix-conversion.java
@@ -0,0 +1,35 @@
+// User function Template for Java
+
+class Solution {
+    static String preToPost(String pre_exp) {
+        // code here
+        String exp=pre_exp;
+        int i=exp.length()-1;
+        Stack<String> st=new Stack<>();
+        String op=new String();
+        while(i>=0)
+        {
+            op=Character.toString(exp.charAt(i));
+            if(exp.charAt(i)>='A'&&exp.charAt(i)<='Z'||
+            exp.charAt(i)>='a'&&exp.charAt(i)<='z'||
+            exp.charAt(i)>='0'&&exp.charAt(i)<='9')
+            {
+                st.push(op);
+            }
+            else
+            {
+                StringBuilder stb=new StringBuilder();
+                String t1=st.peek();
+                st.pop();
+                String t2=st.peek();
+                st.pop();
+                stb.append(t1);
+                stb.append(t2);
+                stb.append(op);
+                st.push(stb.toString());
+            }
+            i--;
+        }
+        return st.peek();
+    }
+}
