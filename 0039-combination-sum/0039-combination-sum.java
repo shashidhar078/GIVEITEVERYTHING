bc0e366d5f0601cc4ba4518f56c6705fe30e2dab
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-03-24 23:14:25 +0530

    Time: 2 ms (95.51%), Space: 45.8 MB (45.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,32 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> res=new ArrayList<>();
-        int n=candidates.length;
-        funCombo(0,candidates,ans,res,n,target);
+        List<Integer> ls=new ArrayList<>();
+        generateCombo(ans,ls,candidates,target,0);
         return ans;
     }
-    public static void funCombo(int i,int[] arr,List<List<Integer>> ans,List<Integer> res,int n,int target)
+    public static void generateCombo(List<List<Integer>> ans,List<Integer> ls,int[] arr,int target,int index)
     {
-        if(i==n)
-        { 
+        if(index==arr.length)
+        {
             if(target==0)
             {
-                ans.add(new ArrayList<>(res));
+                ans.add(new ArrayList<>(ls));
             }
-            
             return;
         }
-        if(arr[i]<=target)
-        {
-             res.add(arr[i]);
-            funCombo(i,arr,ans,res,n,target-arr[i]);
-             res.remove(res.size()-1);
-        }
-        //  funCombo(i+1,arr,ans,res,n,target);
-      
-        
-        funCombo(i+1,arr,ans,res,n,target);
 
+        if(arr[index]<=target)
+        {
+            ls.add(arr[index]);
+            generateCombo(ans,ls,arr,target-arr[index],index);
+            ls.remove(ls.size()-1);
+        }
+        generateCombo(ans,ls,arr,target,index+1);
     }
 }
