f21d808e724be0b99bcfb01b8dabd2a8f5f5b0a4
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-03-26 21:40:21 +0530

    Time: 6 ms (73.5%), Space: 45.2 MB (75.45%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,56 +1,32 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> res=new ArrayList<>();
-        int n=candidates.length;
-        Arrays.sort(candidates);  
-        funCombo(0,ans,res,candidates,n,target);
+        List<Integer> ls=new ArrayList<>();
+        Arrays.sort(candidates);
+        combinationSum(ans,ls,candidates,target,0,0);
         return ans;
     }
-    // public static void funCombo(int i,List<List<Integer>> ans,List<Integer> res,int[] arr,int n,int target,HashSet<List<Integer>> mpp)
-    // {
-    //     if(i==n)
-    //     {
-    //         if(target==0)
-    //         {   
-    //             List<Integer> copy=new ArrayList<>(res);
-    //             if(!mpp.contains(copy))
-    //             {
-    //                 ans.add(copy);
-    //                 mpp.add(copy);
-    //             }
-                
-    //         }
-    //         return;
-    //     }
-    //     if(arr[i]<=target)
-    //     {
-    //         res.add(arr[i]);
-    //         funCombo(i+1,ans,res,arr,n,target-arr[i],mpp);
-    //         res.remove(res.size()-1);
-    //     }
-    //     funCombo(i+1,ans,res,arr,n,target,mpp);
-    // }
-     public static void funCombo(int i,List<List<Integer>> ans,List<Integer> res,int[] arr,int n,int target)
+    public static void combinationSum(List<List<Integer>> ans,List<Integer> ls,int[] arr,int target,int index,int sum)
     {
-       
-            if(target==0)
-            {   
-                List<Integer> copy=new ArrayList<>(res);
-                    ans.add(copy);
-                return;
+        
+            if(sum==target)
+            {
+                ans.add(new ArrayList<>(ls));
             }
-            
-        for(int j=i;j<arr.length;j++)
+           
+        for(int j=index;j<arr.length;j++)
         {
-            if(j>i&&arr[j]==arr[j-1]) continue;
-            if(arr[j]>target) break;
-
-            res.add(arr[j]);
-             funCombo(j+1,ans,res,arr,n,target-arr[j]);
-             res.remove(res.size()-1);
-
+            if(j>index&&arr[j]==arr[j-1])
+            {
+                continue;
+            }
+            if (sum + arr[j] > target) break;
+            ls.add(arr[j]);
+            sum=sum+arr[j];
+            combinationSum(ans,ls,arr,target,j+1,sum);
+            sum=sum-arr[j];
+            ls.remove(ls.size()-1);
         }
-       
-    }
+        
+    } 
 }
