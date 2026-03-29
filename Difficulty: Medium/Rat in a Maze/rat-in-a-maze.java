db5735fd3013d57a88c9954f6bce807775b0121f
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-03-29 20:00:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze/rat-in-a-maze.java
+++ Difficulty: Medium/Rat in a Maze/rat-in-a-maze.java
@@ -1,58 +1,57 @@
 class Solution {
     public ArrayList<String> ratInMaze(int[][] maze) {
         // code here
-        ArrayList<String> res=new ArrayList<>();
         int n=maze.length;
-        int visited[][]=new int[n][n];
-        solve(0,0,maze,res,n,"",visited);
-        return res;            
+        ArrayList<String> ans=new ArrayList<>();
+        int[][] visited=new int[n][n];
+        searchRoute(0,0,ans,maze,"",visited,n);
+        Collections.sort(ans);
+        return ans;
     }
-    public static void solve(int i,int j,int[][] maze,ArrayList<String> res,int n,String ans,int[][] visited)
+    public static void searchRoute(int i,int j,ArrayList<String> ans,int[][] maze,String s,int[][] visited,int n)
     {
         if(i==n-1&&j==n-1)
         {
-            res.add(ans);
+            ans.add(s);
             return;
         }
         
+        
         visited[i][j]=1;
-        //downward
-        if(isSafe(i+1,j,maze,n,visited))
+        //downward direction 
+        
+        if(isSafe(i+1,j,maze,visited,n))
         {
-            solve(i+1,j,maze,res,n,ans+'D',visited);
+            searchRoute(i+1,j,ans,maze,s+"D",visited,n);
         }
         
-        //leftward
-         if(isSafe(i,j-1,maze,n,visited))
+        //upward direction 
+        if(isSafe(i-1,j,maze,visited,n))
         {
-            solve(i,j-1,maze,res,n,ans+'L',visited);
+             searchRoute(i-1,j,ans,maze,s+"U",visited,n);
         }
         
-        //rightward
-        if(isSafe(i,j+1,maze,n,visited))
+        //Right direction
+        if(isSafe(i,j+1,maze,visited,n))
         {
-            solve(i,j+1,maze,res,n,ans+'R',visited);
+            searchRoute(i,j+1,ans,maze,s+"R",visited,n);
         }
         
-        //upward
-        if(isSafe(i-1,j,maze,n,visited))
+        //left direction 
+        if(isSafe(i,j-1,maze,visited,n))
         {
-            solve(i-1,j,maze,res,n,ans+'U',visited);
+             searchRoute(i,j-1,ans,maze,s+"L",visited,n);
         }
-        
-        
-        
-       
         
         visited[i][j]=0;
+                
     }
-    public static boolean  isSafe(int row,int col,int[][] maze,int n,int[][] visited)
+    public static boolean isSafe(int i,int j,int[][] maze,int[][] visited,int n)
     {
-        if(row>=0&&col>=0&&row<=n-1&&col<=n-1&&visited[row][col]==0&&maze[row][col]==1)
+        if(i>=0&&j>=0&&i<=n-1&&j<=n-1&&maze[i][j]==1&&visited[i][j]==0)
         {
             return true;
         }
         return false;
     }
 }
-
