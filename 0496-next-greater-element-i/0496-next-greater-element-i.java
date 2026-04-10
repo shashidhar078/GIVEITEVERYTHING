8997ac3031fd38023d25e76a1b932fe848d5606b
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-04-10 07:20:08 +0530

    Time: 4 ms (74.47%), Space: 45.3 MB (39.8%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,40 +1,39 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-       
-        Stack<Integer> st=new Stack<>();
         HashMap<Integer,Integer> mpp=new HashMap<>();
+        Stack<Integer> st=new Stack<>();
         for(int i=nums2.length-1;i>=0;i--)
         {
-          
+            if(st.isEmpty())
+            {
+                mpp.put(nums2[i],-1);
+            }
+            else if(!st.isEmpty()&&st.peek()>nums2[i])
+            {
+                mpp.put(nums2[i],st.peek());
+            }
+            else if(!st.isEmpty()&&st.peek()<=nums2[i])
+            {
+                while(!st.isEmpty()&&st.peek()<=nums2[i])
+                {
+                    st.pop();
+                }
                 if(st.isEmpty())
                 {
                      mpp.put(nums2[i],-1);
                 }
-                else if(!st.isEmpty()&&st.peek()>nums2[i])
+                else
                 {
-                    mpp.put(nums2[i],st.peek());
+                     mpp.put(nums2[i],st.peek());
                 }
-                else if(!st.isEmpty()&&st.peek()<=nums2[i])
-                {
-                    while(!st.isEmpty()&&st.peek()<=nums2[i])
-                    {
-                        st.pop();
-                    }
-                    if(st.isEmpty())
-                    {
-                        mpp.put(nums2[i],-1);
-                    }
-                    else
-                    {
-                        mpp.put(nums2[i],st.peek());
-                    }
-                }
+            }
             st.push(nums2[i]);
         }
-       int[] a = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            a[i] = mpp.get(nums1[i]); // auto-unboxing
+        int[] ans=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++)
+        {
+            ans[i]=mpp.get(nums1[i]);
         }
-       return a;
+        return ans;
     }
 }
