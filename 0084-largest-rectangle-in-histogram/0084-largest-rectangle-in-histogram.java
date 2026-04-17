1dc59a1cd3802af322af46ce66fb44de3c2b4540
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-04-17 22:43:05 +0530

    Time: 187 ms (5.07%), Space: 82.8 MB (12.85%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,93 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int[] pse=new int[heights.length];
+        int[] nse=new int[heights.length];
+        pse=funPse(heights);
+        nse=funNse(heights);
+        int maxArea=0;
+        int area=0;
+        for(int i=0;i<heights.length;i++)
+        {
+            area=heights[i]*(nse[i]-pse[i]-1);
+            maxArea=Math.max(area,maxArea);
+        }
+        return maxArea;
+    }
+    public static int[] funNse(int[] arr)
+    {
+        Stack<Integer> st=new Stack<>();
+        ArrayList<Integer> res=new ArrayList<>();
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            if(st.isEmpty())
+            {
+                res.add(arr.length);
+            }
+            else if(!st.isEmpty()&&arr[st.peek()]<arr[i])
+            {
+                res.add(st.peek());
+            }
+            else if(!st.isEmpty()&&arr[st.peek()]>=arr[i])
+            {
+                while(!st.isEmpty()&&arr[st.peek()]>=arr[i])
+                {
+                    st.pop();
+                }
+                if(st.isEmpty())
+                {
+                    res.add(arr.length);
+                }
+                else
+                {
+                    res.add(st.peek());
+                }
+            }
+            st.push(i);
+        }
+        Collections.reverse(res);
+        int[] fres=new int[arr.length];
+        for(int i=0;i<arr.length;i++)
+        {
+            fres[i]=res.get(i);
+        }
+        return fres;
+    }
+    public static int[] funPse(int[] arr)
+    {
+        Stack<Integer> st=new Stack<>();
+        ArrayList<Integer> res=new ArrayList<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            if(st.isEmpty())
+            {
+                res.add(-1);
+            }
+            else if(!st.isEmpty()&&arr[st.peek()]<arr[i])
+            {
+                res.add(st.peek());
+            }
+            else if(!st.isEmpty()&&arr[st.peek()]>=arr[i])
+            {
+                while(!st.isEmpty()&&arr[st.peek()]>=arr[i])
+                {
+                    st.pop();
+                }
+                if(st.isEmpty())
+                {
+                    res.add(-1);
+                }
+                else
+                {
+                    res.add(st.peek());
+                }
+            }
+            st.push(i);
+        }
+        int[] fres=new int[arr.length];
+        for(int i=0;i<arr.length;i++)
+        {
+            fres[i]=res.get(i);
+        }
+        return fres;
+    }
+}
