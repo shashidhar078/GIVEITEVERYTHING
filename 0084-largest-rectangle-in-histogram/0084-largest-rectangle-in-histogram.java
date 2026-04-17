bf37df1c5da99a1ac10f30e5fd86b766dd3586bb
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-04-17 23:11:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,93 +1,124 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] pse=new int[heights.length];
-        int[] nse=new int[heights.length];
-        pse=funPse(heights);
-        nse=funNse(heights);
+        // int[] pse=new int[heights.length];
+        // int[] nse=new int[heights.length];
+        // pse=funPse(heights);
+        // nse=funNse(heights);
+        // int maxArea=0;
+        // int area=0;
+        // for(int i=0;i<heights.length;i++)
+        // {
+        //     area=heights[i]*(nse[i]-pse[i]-1);
+        //     maxArea=Math.max(area,maxArea);
+        // }
+        // return maxArea;
+        //optimal solution
+        Stack<Integer> st=new Stack<>();
         int maxArea=0;
         int area=0;
+        int pse=0;
+        int nse=0;
+        int n=heights.length;
         for(int i=0;i<heights.length;i++)
         {
-            area=heights[i]*(nse[i]-pse[i]-1);
+            while(!st.isEmpty()&&heights[st.peek()]>heights[i])
+            {
+                int ele=st.peek();
+                st.pop();
+                pse=st.isEmpty()?-1:st.peek();
+                area=heights[ele]*(i-pse-1);
+                maxArea=Math.max(area,maxArea);
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty())
+        {
+            nse=n;
+            int ele=st.peek();
+            st.pop();
+            pse=st.isEmpty()?-1:st.peek();
+            area=heights[ele]*(nse-pse-1);
             maxArea=Math.max(area,maxArea);
+            
         }
         return maxArea;
     }
-    public static int[] funNse(int[] arr)
-    {
-        Stack<Integer> st=new Stack<>();
-        ArrayList<Integer> res=new ArrayList<>();
-        for(int i=arr.length-1;i>=0;i--)
-        {
-            if(st.isEmpty())
-            {
-                res.add(arr.length);
-            }
-            else if(!st.isEmpty()&&arr[st.peek()]<arr[i])
-            {
-                res.add(st.peek());
-            }
-            else if(!st.isEmpty()&&arr[st.peek()]>=arr[i])
-            {
-                while(!st.isEmpty()&&arr[st.peek()]>=arr[i])
-                {
-                    st.pop();
-                }
-                if(st.isEmpty())
-                {
-                    res.add(arr.length);
-                }
-                else
-                {
-                    res.add(st.peek());
-                }
-            }
-            st.push(i);
-        }
-        Collections.reverse(res);
-        int[] fres=new int[arr.length];
-        for(int i=0;i<arr.length;i++)
-        {
-            fres[i]=res.get(i);
-        }
-        return fres;
-    }
-    public static int[] funPse(int[] arr)
-    {
-        Stack<Integer> st=new Stack<>();
-        ArrayList<Integer> res=new ArrayList<>();
-        for(int i=0;i<arr.length;i++)
-        {
-            if(st.isEmpty())
-            {
-                res.add(-1);
-            }
-            else if(!st.isEmpty()&&arr[st.peek()]<arr[i])
-            {
-                res.add(st.peek());
-            }
-            else if(!st.isEmpty()&&arr[st.peek()]>=arr[i])
-            {
-                while(!st.isEmpty()&&arr[st.peek()]>=arr[i])
-                {
-                    st.pop();
-                }
-                if(st.isEmpty())
-                {
-                    res.add(-1);
-                }
-                else
-                {
-                    res.add(st.peek());
-                }
-            }
-            st.push(i);
-        }
-        int[] fres=new int[arr.length];
-        for(int i=0;i<arr.length;i++)
-        {
-            fres[i]=res.get(i);
-        }
-        return fres;
-    }
+    //brute force approach
+    // public static int[] funNse(int[] arr)
+    // {
+    //     Stack<Integer> st=new Stack<>();
+    //     ArrayList<Integer> res=new ArrayList<>();
+    //     for(int i=arr.length-1;i>=0;i--)
+    //     {
+    //         if(st.isEmpty())
+    //         {
+    //             res.add(arr.length);
+    //         }
+    //         else if(!st.isEmpty()&&arr[st.peek()]<arr[i])
+    //         {
+    //             res.add(st.peek());
+    //         }
+    //         else if(!st.isEmpty()&&arr[st.peek()]>=arr[i])
+    //         {
+    //             while(!st.isEmpty()&&arr[st.peek()]>=arr[i])
+    //             {
+    //                 st.pop();
+    //             }
+    //             if(st.isEmpty())
+    //             {
+    //                 res.add(arr.length);
+    //             }
+    //             else
+    //             {
+    //                 res.add(st.peek());
+    //             }
+    //         }
+    //         st.push(i);
+    //     }
+    //     Collections.reverse(res);
+    //     int[] fres=new int[arr.length];
+    //     for(int i=0;i<arr.length;i++)
+    //     {
+    //         fres[i]=res.get(i);
+    //     }
+    //     return fres;
+    // }
+    // public static int[] funPse(int[] arr)
+    // {
+    //     Stack<Integer> st=new Stack<>();
+    //     ArrayList<Integer> res=new ArrayList<>();
+    //     for(int i=0;i<arr.length;i++)
+    //     {
+    //         if(st.isEmpty())
+    //         {
+    //             res.add(-1);
+    //         }
+    //         else if(!st.isEmpty()&&arr[st.peek()]<arr[i])
+    //         {
+    //             res.add(st.peek());
+    //         }
+    //         else if(!st.isEmpty()&&arr[st.peek()]>=arr[i])
+    //         {
+    //             while(!st.isEmpty()&&arr[st.peek()]>=arr[i])
+    //             {
+    //                 st.pop();
+    //             }
+    //             if(st.isEmpty())
+    //             {
+    //                 res.add(-1);
+    //             }
+    //             else
+    //             {
+    //                 res.add(st.peek());
+    //             }
+    //         }
+    //         st.push(i);
+    //     }
+    //     int[] fres=new int[arr.length];
+    //     for(int i=0;i<arr.length;i++)
+    //     {
+    //         fres[i]=res.get(i);
+    //     }
+    //     return fres;
+    // }
 }
