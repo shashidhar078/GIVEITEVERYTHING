40c7feda68025332b11fd4cc6f1fc237830a452c
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-04-19 12:08:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,26 +1,47 @@
 class StockSpanner {
 
      ArrayList<Integer> arr;
+    Stack<Integer> st;
+    int i=0;
     public StockSpanner() {
        arr=new ArrayList<>();
-        // StockSpanner sp=new StockSpanner();
+            st=new Stack<>();
+       
     }
     
     public int next(int price) {
+        // arr.add(price);
+        // int count=1;
+        // int num=price;
+        // for(int i=arr.size()-2;i>=0;i--)
+        // {
+        //     if(arr.get(i)<=num)
+        //     {
+        //         count++;
+        //     }
+        //     else
+        //     {
+        //         break;
+        //     }
+        // }
+        // return count;
+        
+        int count=0;
         arr.add(price);
-        int count=1;
-        int num=price;
-        for(int i=arr.size()-2;i>=0;i--)
+        // if(st.isEmpty())
+        // {
+        //     st.add(i);
+        // }
+        while(!st.isEmpty()&&price>=arr.get(st.peek()))
         {
-            if(arr.get(i)<=num)
-            {
-                count++;
-            }
-            else
-            {
-                break;
-            }
+            st.pop();
         }
+        count=st.isEmpty()?1:i-st.peek();
+        if(st.isEmpty()||arr.get(st.peek())>price)
+        {
+            st.push(i);
+        }
+        i++;
         return count;
     }
 }
