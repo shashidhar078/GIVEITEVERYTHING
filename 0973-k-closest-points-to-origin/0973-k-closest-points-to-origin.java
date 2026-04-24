74398e2fd2a076593068c85ce5ed31c1b8da2940
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-04-25 00:46:57 +0530

    Time: 30 ms (64.81%), Space: 53.7 MB (80.43%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -3,34 +3,45 @@ class Solution {
         // step1 : compute the distance , avoid squaring the values , distance formula root of x2+y2
         
         //step2:build priority queue upto ele k which results O(nlogn)
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->(a[0]*a[0]+a[1]*a[1])-(b[0]*b[0]+b[1]*b[1]));
+        // PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->(a[0]*a[0]+a[1]*a[1])-(b[0]*b[0]+b[1]*b[1]));
+       
+
         // for(int i=0;i<points.length;i++)
         // {
-        //     x=points[i][0];
-        //     y=points[i][1];
-
-        //     int ans=(int)Math.pow(x,2)+(int)Math.pow(y,2);
-        //     pq.add(ans);
-        //     hs.put(ans,points[i]);
+        //     pq.add(points[i]);
+        //     // if(pq.size()>k)
+        //     // {
+        //     //     pq.remove();
+        //     // }
         // }
+        // int[][] res=new int[k][2];
 
-        for(int i=0;i<points.length;i++)
+        // for(int i=0;i<k;i++)
+        // {
+        //     // res[i]=pq.peek();
+        //     // pq.remove();
+        //     res[i]=pq.poll();
+        // }
+        // return res;
+
+
+        //optimized version 
+
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->(b[0]*b[0]+b[1]*b[1])-(a[0]*a[0]+a[1]*a[1]));
+    
+        for(int[] i:points)
         {
-            pq.add(points[i]);
+            pq.add(i);
             if(pq.size()>k)
             {
-                pq.remove();
+                pq.poll();
             }
         }
         int[][] res=new int[k][2];
-
         for(int i=0;i<k;i++)
         {
-            // res[i]=pq.peek();
-            // pq.remove();
             res[i]=pq.poll();
         }
         return res;
-    }
-
+}
 }
