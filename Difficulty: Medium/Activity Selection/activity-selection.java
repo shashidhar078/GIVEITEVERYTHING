9d4ca6f6d32525c32334b27c209b152464b7e0d8
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-05-25 22:10:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Activity Selection/activity-selection.java
+++ Difficulty: Medium/Activity Selection/activity-selection.java
@@ -1,32 +1,31 @@
 class Solution {
     public int activitySelection(int[] start, int[] finish) {
         // code here
-        int maxAct=0;
-        
-        //we are considering from activity 0 so initially activity count is added already        
-        maxAct=1;
+        int count=0;
         
         int[][] activities=new int[start.length][3];
         
-        for(int i=0;i<start.length;i++)
+        for(int i=0;i<activities.length;i++)
         {
             activities[i][0]=i;
             activities[i][1]=start[i];
             activities[i][2]=finish[i];
         }
         
+        Arrays.sort(activities,Comparator.comparingDouble(o->o[2]));
         
-        Arrays.sort(activities,Comparator.comparingInt(o->o[2]));
         
-        int lastVal=activities[0][2];
-        for(int i=1;i<activities.length;i++)
+        
+        int finished=activities[0][2];
+        count++;
+        for(int i=1;i<finish.length;i++)
         {
-            if(activities[i][1]>lastVal)
+            if(activities[i][1]>finished)
             {
-                maxAct++;
-                lastVal=activities[i][2];
+                count++;
+                finished=activities[i][2];
             }
         }
-        return maxAct;
+        return count;
     }
 }
