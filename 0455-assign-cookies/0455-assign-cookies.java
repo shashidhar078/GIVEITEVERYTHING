56e0100df78aa6ede465a14a8411b22f5def1669
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-05-26 22:05:11 +0530

    Time: 14 ms (94.07%), Space: 51.4 MB (72.4%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,28 +1,21 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        //lets make it simple with two pointers approach
-        //by sorting the two arrays so we could assign cookies properly
-
+        //lets make it simple by appying simple sorting technique and two pointers approach 
 
         Arrays.sort(g);
         Arrays.sort(s);
 
-        int l=0;
-        int r=0;
-
-        int m=g.length;
-        int n=s.length;
-
         int count=0;
-        while(l<n&&r<m)
+        int ls=0;
+        int rg=0;
+        while(ls<s.length&&rg<g.length)
         {
-            if(s[l]>=g[r])
+            if(s[ls]>=g[rg])
             {
                 count++;
-                r++;
-                // l++;
+                rg++;
             }
-            l++;
+            ls++;
         }
         return count;
     }
