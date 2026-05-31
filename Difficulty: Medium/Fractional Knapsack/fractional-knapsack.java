7ead58523a5326efb7da7d30e85fbaa69420d7bd
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-05-31 17:35:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.java
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.java
@@ -1,23 +1,28 @@
 class Solution {
     public double fractionalKnapsack(int[] val, int[] wt, int capacity) {
         // code here
+        double[][] act=new double[wt.length][3];
+        
         double value=0.0;
-        double[][] act=new double[val.length][3];
         
         for(int i=0;i<wt.length;i++)
         {
             act[i][0]=val[i];
             act[i][1]=wt[i];
-            act[i][2]=(double) val[i]/wt[i];
+            act[i][2]=(double)val[i]/wt[i];
         }
         
-        Arrays.sort(act, (a,b) -> Double.compare(b[2], a[2]));        
+        
+        
+        Arrays.sort(act,(a,b)->Double.compare(b[2],a[2]));
+        
+        
         for(int i=0;i<wt.length;i++)
         {
-            if(capacity>=act[i][1])
+            if(act[i][1]<=capacity)
             {
-                value+=act[i][0];
-                capacity-=act[i][1];
+                value=value+act[i][0];
+                capacity=capacity-(int)act[i][1];
             }
             else
             {
