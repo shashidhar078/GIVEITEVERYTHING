efb04a6d5c80347aa16f8688efc778f392c07d7d
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-06-12 21:24:26 +0530

    Time: 1 ms (42.36%), Space: 47 MB (70.26%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,33 +14,48 @@
  * }
  */
 class Solution {
+     int diameter=0;
     public int diameterOfBinaryTree(TreeNode root) {
-        int res=calDiam(root);
-        return res;
+        int res=calHeight(root);
+        return diameter;
     }
-    public static int calDiam(TreeNode root)
+    //This approach will take O(n2)
+    // public static int calDiam(TreeNode root)
+    // {
+    //     if(root==null)
+    //     {
+    //         return 0;
+    //     }
+    //     int ld=calDiam(root.left);
+    //     int lh=height(root.left);
+    //     int rd=calDiam(root.right);
+    //     int rh=height(root.right);
+    //     int selfDiam=rh+lh;
+    //     return Math.max(selfDiam,Math.max(ld,rd));
+    // }
+    // public static int height(TreeNode root)
+    // {
+    //     if(root==null)
+    //     {
+    //         return 0;
+    //     }
+    //     int lh=height(root.left);
+    //     int rh=height(root.right);
+    //     return Math.max(lh,rh)+1;
+    // }
+   
+    //we will calculate directly height and self diam with single dfs pass only 
+
+    public int calHeight(TreeNode root)
     {
         if(root==null)
         {
             return 0;
         }
-        int ld=calDiam(root.left);
-        int lh=height(root.left);
-        int rd=calDiam(root.right);
-        int rh=height(root.right);
-        int selfDiam=rh+lh;
-        return Math.max(selfDiam,Math.max(ld,rd));
-    }
-    public static int height(TreeNode root)
-    {
-        if(root==null)
-        {
-            return 0;
-        }
-        int lh=height(root.left);
-        int rh=height(root.right);
+        int lh=calHeight(root.left);
+        int rh=calHeight(root.right);
+        diameter=Math.max(diameter,lh+rh); //self root diam implicit
         return Math.max(lh,rh)+1;
     }
-   
-   
+
 }
