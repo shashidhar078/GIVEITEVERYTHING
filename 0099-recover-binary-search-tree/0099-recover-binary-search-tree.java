1410048b98cfb89c161db66fbe4eeba88085a65d
Shashidhar Nagunuri <153267256+shashidhar078@users.noreply.github.com>
2026-07-25 20:05:42 +0530

    Time: 1 ms (100%), Space: 46.3 MB (87.22%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -14,35 +14,46 @@
  * }
  */
 class Solution {
+    TreeNode first=null;
+    TreeNode second=null;
+    TreeNode prev=null;
     public void recoverTree(TreeNode root) {
-        ArrayList<TreeNode> arr=new ArrayList<>();
-        inorder(root,arr);
-        TreeNode first=null;
-        TreeNode second=null;
-        for(int i=1;i<arr.size();i++)
-        {
-            if(arr.get(i-1).val>arr.get(i).val)
-            {
-                if(first==null)
-                {
-                    first=arr.get(i-1);
-                }
-                second=arr.get(i);
-            }
-        }
-
+        // ArrayList<TreeNode> arr=new ArrayList<>();
+        // inorder(root,arr);
+        // TreeNode first=null;
+        // TreeNode second=null;
+        // for(int i=1;i<arr.size();i++)
+        // {
+        //     if(arr.get(i-1).val>arr.get(i).val)
+        //     {
+        //         if(first==null)
+        //         {
+        //             first=arr.get(i-1);
+        //         }
+        //         second=arr.get(i);
+        //     }
+        // }
+        inorder(root);
         int temp=first.val;
         first.val=second.val;
         second.val=temp;
     }
-    public static void inorder(TreeNode root,ArrayList<TreeNode> arr)
+    public void inorder(TreeNode root)
     {
         if(root==null)
         {
             return;
         }
-        inorder(root.left,arr);
-        arr.add(root);
-        inorder(root.right,arr);
+        inorder(root.left);
+        if(prev!=null&&prev.val>root.val)
+        {
+            if(first==null)
+            {
+                first=prev;
+            }
+            second=root;
+        }
+        prev=root;
+        inorder(root.right);
     }
 }
